bbca9ab44a9194c4813fa3c81fb2e5374388da19
AthN3090 <amandevrishi12@gmail.com>
2023-09-26 15:03:18 +0530

    horizontal scrolling projects testing-cards added

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -73,6 +73,35 @@ function Work() {
                   </div>
                 </div>
                 <div className="w-full shrink-0">
+                  <div className="w-[1280px] shrink-0 mx-auto flex gap-[150px] justify-between items-center h-full top-1/2">
+                    <div className="flex justify-center items-center">
+                      <Image
+                        src="/images/project1.png"
+                        height={766}
+                        width={864}
+                        alt="project1"
+                      />
+                    </div>
+                    <div className="max-w-[557px]">
+                      <p className="font-medium text-4xl mb-8">You Matter</p>
+                      <p className="font-semibold text-5xl mb-6">
+                        Build and design your routine with ease{" "}
+                      </p>
+                      <p className="font-medium text-xl mb-20">
+                        Crafting delightful, human centred and inclusive
+                        experiences. Crafting delightful, human centred and
+                        inclusive experiences. Crafting delightful, human
+                        centred and inclusive experiences. Crafting delightful,
+                        human centred and inclusive experiences.{" "}
+                      </p>
+
+                      <button className="py-3 px-5 border border-[#828282] flex gap-5">
+                        Read case study -{">"}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full shrink-0">
                   <div ref={card} className="w-[1280px] shrink-0 mx-auto flex gap-[150px] justify-between items-center h-full top-1/2">
                     <div className="flex justify-center items-center">
                       <Image
