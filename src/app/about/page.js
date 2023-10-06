6ffbf4b801acfd6ee1bc8866d5b30b55c81bfea9
AthN3090 <amandevrishi12@gmail.com>
2023-10-06 23:27:14 +0530

    about section resume button margin adjusted

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -17,36 +17,41 @@ function About() {
               A little bit about myself
             </p>
             <p className="mt-4 leading-7 ">
-              My name, Twisha, carries the beautiful meaning of &quot;divine aura.&quot; I
-              am a self-taught UX designer based in Gujarat, India. My journey
-              in this field has been quite interesting. Even though I studied
-              computer science, I soon discovered that my real love is UX
-              Design. It&apos;s the perfect mix of solving problems and being
-              creative.<br></br>
+              My name, Twisha, carries the beautiful meaning of &quot;divine
+              aura.&quot; I am a self-taught UX designer based in Gujarat,
+              India. My journey in this field has been quite interesting. Even
+              though I studied computer science, I soon discovered that my real
+              love is UX Design. It&apos;s the perfect mix of solving problems
+              and being creative.<br></br>
               <br></br>
               My heart truly lies in crafting seamless and delightful user
               experiences. I&apos;m wholeheartedly dedicated to solving problems
               through design. I am constantly on the lookout for opportunities
               to learn and grow. <br></br>
               <br></br>
-              When I&apos;m not designing, you’ll often find me creating some sort of art, solving puzzles, or or simply enjoying with dogs. Apart from this, I swim, cycle, and play badminton to stay active. <br></br>
+              When I&apos;m not designing, you’ll often find me creating some
+              sort of art, solving puzzles, or or simply enjoying with dogs.
+              Apart from this, I swim, cycle, and play badminton to stay active.{" "}
               <br></br>
-              Thank you for taking the time to get to know me a little better. 🖤
+              <br></br>
+              Thank you for taking the time to get to know me a little better.
+              🖤
             </p>
-
-            <a
-              href="#"
-              className="flex items-center transition-all gap-3 hover:gap-5 hover:underline mt-10 border w-fit p-3 border-[#828282]"
-            >
-              {" "}
-              <span className="mt-[2px]">View my resume </span>
-              <Image
-                src={"/images/right.svg"}
-                alt="right-arrow"
-                height={16}
-                width={16}
-              ></Image>{" "}
-            </a>
+            <div className="flex justify-center items-center lg:justify-normal">
+              <a
+                href="#"
+                className="flex items-center transition-all gap-3 hover:gap-5 hover:underline my-10 border w-fit p-3 border-[#828282]"
+              >
+                {" "}
+                <span className="mt-[2px]">View my resume </span>
+                <Image
+                  src={"/images/right.svg"}
+                  alt="right-arrow"
+                  height={16}
+                  width={16}
+                ></Image>{" "}
+              </a>
+            </div>
           </div>
         </div>
       </main>
